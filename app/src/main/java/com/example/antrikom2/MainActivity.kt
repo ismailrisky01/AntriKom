615d43020c025deb968d0d84b8ba70e949f30e6c
ismailrisky <ismailrisky01@gmail.com>
2021-04-30 09:37:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/antrikom2/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.antrikom2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.antrikom2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var listener: NavController.OnDestinationChangedListener
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Setup ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Setup Navigation Slide
+        navController = findNavController(R.id.nav_host_fragment)
+        drawerLayout = binding.drawerLayout
+        binding.navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/antrikom2/ui/pengajuan/PengajuanFragment.kt
@@ -0,0 +1,41 @@
+package com.example.antrikom2.ui.pengajuan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
+import com.example.antrikom2.R
+import com.example.antrikom2.databinding.FragmentPengajuanBinding
+
+
+class PengajuanFragment : Fragment() {
+    private var _binding: FragmentPengajuanBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPengajuanBinding.inflate(inflater,container,false)
+        val languages = resources.getStringArray(R.array.list)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, languages)
+        binding.autoCompleteTextView2.setAdapter(arrayAdapter)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.IDPengajuanButtonAmbilAntrian.setOnClickListener {
+            findNavController().navigate(R.id.action_pengajuanFragment_to_antrianFragment)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding =null
+    }
+
+
+}
